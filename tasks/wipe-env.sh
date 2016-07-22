a79c93238a92ad183c15b9bf4c21a37c4fdd8e7a
virtmerlin <merlin@sense.net>
2016-07-22 17:50:39 -0500

    ci skeleton

---
+++ tasks/wipe-env.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#set -e
+
+
+if [ $arg_wipe == "wipe" ];
+        then
+                echo "Wiping Environment...."
+        else
+                echo "Need Args [0]=wipe "
+                echo "Example: ./p1-0-wipe-env.sh wipe ..."
+                exit 1
+fi
+
+
+#################### GCP Begin ##########################
+## Stopid Reqs ...
+## Create a svc account & enable the API by hand
+
+
+echo $gcp_svc_acct_key > /tmp/blah
+
+gcloud auth activate-service-account --key-file /tmp/blah
+rm -rf /tmp/blah
+
+gcloud config set project $gcp_proj_id
+
+for i in $(gcloud compute instances list | grep c0-run1 | awk '{print $1}'); do
+
+	 gcloud compute instances delete $i --delete-disks all;
+
+done
+
+#################### GCP End   ##########################
