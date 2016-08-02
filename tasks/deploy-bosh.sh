e0f9dd73c533d0482046d65d3e18d41eafa03672
virtmerlin <merlin@sense.net>
2016-08-02 14:25:14 -0500

    UPDATE: deploy-bosh.sh add wipe for old keys

--- tasks/deploy-bosh.sh
+++ tasks/deploy-bosh.sh
@@ -42,11 +42,14 @@ echo "Deploying BOSH ..."
 gcloud compute copy-files ${bosh_manifest} ${gcp_terraform_prefix}-bosh-bastion:/home/bosh --zone ${gcp_zone_1} --quiet
 # Gen BOSH instance SSH Keys on Bastion
 gcloud compute ssh ${gcp_terraform_prefix}-bosh-bastion \
---command "cd /home/bosh && ssh-keygen -t rsa -f bosh_key -P '' -C '' && chmod 400 bosh_key" \
+--command "cd /home/bosh && \
+ssh-keygen -t rsa -f bosh_key -P '' -C '' &&
+chmod 400 bosh_key" \
 --zone ${gcp_zone_1}
 # Start bosh-init deploy on Bastion
 gcloud compute ssh ${gcp_terraform_prefix}-bosh-bastion \
---command "cd /home/bosh && if [ -f /home/bosh/bosh-init-state.json ]; then rm -rf /home/bosh/bosh-init-state.json ; fi && \
+--command "cd /home/bosh && \
+if [ -f /home/bosh/bosh_key ]; then rm -rf /home/bosh/bosh_key* ; fi && \
 bosh-init deploy /home/bosh/bosh-init.yml" \
 --zone ${gcp_zone_1}
 
