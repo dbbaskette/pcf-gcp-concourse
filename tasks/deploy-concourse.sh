2e6c05eda8ccc82e27d58996e43f2f6d47c86162
virtmerlin <merlin@sense.net>
2016-08-03 16:01:07 -0500

    UPDATE: deploy-concourse.sh testing multi vars in pipleline

--- tasks/deploy-concourse.sh
+++ tasks/deploy-concourse.sh
@@ -40,6 +40,7 @@ for z in ${CC_RELEASES[@]}; do
   FILE_NAME=$(echo $z | awk -F "/" '{print$NF}')
   fn_gcp_ssh "wget $z -O ~/concourse-releases/$FILE_NAME"
 done
+unset FILE_NAME
 
 #############################################################
 ########## Install omg-cli on Bastion  ######################
@@ -49,9 +50,36 @@ echo "Installing $OMG_CLI to Bastion ..."
 fn_gcp_ssh "wget $OMG_CLI -O /sbin/omg-cli" root
 fn_gcp_ssh "chmod 755 /sbin/omg-cli" root
 
+OMG_PRODUCT_CC="https://github.com$(wget -q -O- https://github.com/enaml-ops/omg-product-bundle/releases/latest | grep concourse-plugin-linux | awk -F '"' '{print$2}')"
+echo "Installing OMG-CLI product $OMG_PRODUCT_CC to Bastion ..."
+CC_ENAML_PLUGIN_FILE_NAME=$(echo $OMG_PRODUCT_CC | awk -F "/" '{print$NF}')
+fn_gcp_ssh "wget $OMG_PRODUCT_CC -O ~/$CC_ENAML_PLUGIN_FILE_NAME"
+fn_gcp_ssh "omg-cli register-plugin --type product --pluginpath ~/$CC_ENAML_PLUGIN_FILE_NAME"
 
-exit 1
-#wget https://github.com/enaml-ops/omg-product-bundle/releases/download/v0.0.8/concourse-plugin-linux
-#wget https://github.com/enaml-ops/omg-cli/releases/download/v0.0.10/aws-cloudconfigplugin-linux
+#############################################################
+########## generate manifest for Concourse w/ ENAML  ########
+#############################################################
+OMG_CC_DEPLOY_CMD="omg-cli deploy-cloudconfig \
+--bosh-url https://${gcp_terraform_subnet_bosh_static} \
+--bosh-port 25555 \
+--bosh-user ${bosh_director_user} \
+--bosh-pass ${bosh_director_password} \
+--ssl-ignore \
+--print-manifest \
+$CC_ENAML_PLUGIN_FILE_NAME \
+--web-vm-type small \
+--worker-vm-type small \
+--database-vm-type small \
+--network-name private \
+--url my.concourse.com \
+--username concourse \
+--password concourse \
+--web-instances 1 \
+--web-azs us-east-1c \
+--worker-azs us-east-1c \
+--database-azs us-east-1c \
+--bosh-stemcell-alias trusty \
+--postgresql-db-pwd secret \
+--database-storage-type medium"
 
-#omg-cli register-plugin --type cloudconfig --pluginpath aws-cloudconfigplugin-linux
+#fn_gcp_ssh "$OMG_CC_DEPLOY_CMD"
