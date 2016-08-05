32c0ea342d0fbe8a17da0bfdbf20d6c568e7c087
virtmerlin <merlin@sense.net>
2016-08-05 10:06:47 -0500

    UPDATE: pipeline.yml, prams, & pcf.tf to add vault network,  also made all terraform script vars == the concourse params/vars

--- tasks/terraform-iaas.sh
+++ tasks/terraform-iaas.sh
@@ -20,16 +20,16 @@ export PATH=/opt/terraform/terraform:$PATH
   pcf-gcp-concourse/terraform/pcf
 
   /opt/terraform/terraform apply \
-    -var "project=$gcp_proj_id" \
-    -var "region=$gcp_region" \
-    -var "resource-prefix=$gcp_terraform_prefix" \
-    -var "bosh-subnet-cidr-range=$gcp_terraform_subnet_bosh" \
-    -var "zone1=$gcp_zone_1" \
-    -var "zone2=$gcp_zone_2" \
-    -var "concourse-subnet-cidr-range=$gcp_terraform_subnet_concourse" \
-    -var "vault-subnet-cidr-range=$gcp_terraform_subnet_vault" \
-    -var "pcf-subnet-zone1-cidr-range=$gcp_terraform_subnet_pcf_zone1" \
-    -var "pcf-subnet-zone2-cidr-range=$gcp_terraform_subnet_pcf_zone2" \
-    -var "sys-domain=$pcf_ert_sys_domain" \
-    -var "key-json=$gcp_svc_acct_key" \
+    -var "gcp_proj_id=$gcp_proj_id" \
+    -var "gcp_region=$gcp_region" \
+    -var "gcp_terraform_prefix=$gcp_terraform_prefix" \
+    -var "gcp_terraform_subnet_bosh=$gcp_terraform_subnet_bosh" \
+    -var "gcp_zone_1=$gcp_zone_1" \
+    -var "gcp_zone_2=$gcp_zone_2" \
+    -var "gcp_terraform_subnet_concourse=$gcp_terraform_subnet_concourse" \
+    -var "gcp_terraform_subnet_vault=$gcp_terraform_subnet_vault" \
+    -var "gcp_terraform_subnet_pcf_zone1=$gcp_terraform_subnet_pcf_zone1" \
+    -var "gcp_terraform_subnet_pcf_zone2=$gcp_terraform_subnet_pcf_zone2" \
+    -var "pcf_ert_sys_domain=$pcf_ert_sys_domain" \
+    -var "gcp_svc_acct_key=$gcp_svc_acct_key" \
     pcf-gcp-concourse/terraform/pcf
